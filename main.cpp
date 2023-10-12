5f4f191ac773e7d72668eecb6884d6ad2f1b2cca
Levente Löffler <levente.loffler99@gmail.com>
2023-10-12 14:47:24 +0200

    Add naiv miller-rabin primality test

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <concepts>
 
 // Only supports non-negative exponents.
-template<std::integral T>
+template <std::integral T>
 [[nodiscard]] auto FastPow(T base, T exp) noexcept -> T {
   if (exp <= 0) {
     return 1;
@@ -23,8 +23,61 @@ template<std::integral T>
   return base * mul;
 }
 
-int main() {
-  for (auto i{0}; i < 50; i++) {
-    std::cout << FastPow<long long>(2, i) << '\n';
+template <std::integral T>
+[[nodiscard]] auto MillerRabin(T const p) noexcept -> bool {
+  if (p <= 1) {
+    return false;
+  }
+
+  if (p == 2) {
+    return true;
+  }
+
+  if (p % 2 == 0) {
+    return false;
+  }
+
+  auto const r{
+    [tmp = p - 1]() mutable {
+      T ret{0};
+
+      while (tmp % 2 == 0) {
+        ++ret;
+        tmp /= 2;
+      }
+
+      return ret;
+    }()
+  };
+
+  auto const m{(p - 1) / FastPow<T>(2, r)};
+
+  for (T a{2}; a < p; ++a) {
+    bool pass{false};
+
+    auto prev{p - 1};
+
+    for (T i{0}; i <= r; ++i) {
+      auto cur{FastPow(a, m * FastPow(2, i)) % p};
+
+      if (cur == 1 && prev == p - 1) {
+        pass = true;
+        break;
+      }
+
+      prev = cur;
+    }
+
+    if (!pass) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+auto main() -> int {
+  for (auto i{3}; i <= 7; i++) {
+    std::cout << i << ": " << MillerRabin(i) << '\n';
   }
 }
