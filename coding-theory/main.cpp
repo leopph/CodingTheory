c73874f4e78a86f5bb59e59dd12bbd91b8eed068
Levente Löffler <levente.loffler99@gmail.com>
2023-11-16 13:29:13 +0100

    Replace templates with 64 bit ints

--- coding-theory/main.cpp
+++ coding-theory/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <concepts>
 #include <random>
+#include <cstdint>
 
 // Only supports non-negative exponents.
-template <std::integral T>
-[[nodiscard]] auto FastPow(T base, T exp) noexcept -> T {
+[[nodiscard]] static auto FastPow(std::int64_t base, std::int64_t exp) noexcept -> std::int64_t {
   if (exp <= 0) {
     return 1;
   }
 
-  T mul{1};
+  std::int64_t mul{1};
 
   while (exp > 1) {
     if (exp % 2 == 1) {
@@ -25,13 +24,12 @@ template <std::integral T>
 }
 
 // Only supports non-negative exponents.
-template <std::integral T>
-[[nodiscard]] auto FastModPow(T base, T exp, T mod) noexcept -> T {
+[[nodiscard]] static auto FastModPow(std::int64_t base, std::int64_t exp, std::int64_t mod) noexcept -> std::int64_t {
   if (mod == 1) {
     return 0;
   }
 
-  T ret{1};
+  std::int64_t ret{1};
   base %= mod;
 
   while (exp > 0) {
@@ -62,9 +60,7 @@ enum class MillerRabinMode {
  * \param randomWitnessCount Specifies how many random witnesses are tested.
  * \return Whether the number is a (potential) prime.
  */
-template <std::integral T>
-[[nodiscard]] auto MillerRabin(T const p, MillerRabinMode const mode = MillerRabinMode::TestAll,
-                               T const randomWitnessCount = 10) noexcept -> bool {
+[[nodiscard]] static auto MillerRabin(std::int64_t const p, MillerRabinMode const mode = MillerRabinMode::TestAll, std::int64_t const randomWitnessCount = 10) noexcept -> bool {
   if (p <= 1) {
     return false;
   }
@@ -79,7 +75,7 @@ template <std::integral T>
 
   auto const r{
     [tmp = p - 1]() mutable {
-      T ret{0};
+      std::int64_t ret{0};
 
       while (tmp % 2 == 0) {
         ++ret;
@@ -90,14 +86,14 @@ template <std::integral T>
     }()
   };
 
-  auto const m{(p - 1) / FastPow<T>(2, r)};
+  auto const m{(p - 1) / FastPow(2, r)};
 
   auto const testWitness{
-    [p, r, m](T const a) {
+    [p, r, m](std::int64_t const a) {
       auto prev{p - 1};
 
-      for (T i{0}; i <= r; ++i) {
-        auto cur{FastModPow<T>(a, m * FastPow<T>(2, i), p)};
+      for (std::int64_t i{0}; i <= r; ++i) {
+        auto cur{FastModPow(a, m * FastPow(2, i), p)};
 
         if (cur == 1 && prev == p - 1) {
           return true;
@@ -111,18 +107,18 @@ template <std::integral T>
   };
 
   if (mode == MillerRabinMode::TestAll) {
-    for (T a{2}; a < p; ++a) {
+    for (std::int64_t a{2}; a < p; ++a) {
       if (!testWitness(a)) {
         return false;
       }
     }
   } else {
-    std::uniform_int_distribution<T> dist{2, p - 1};
+    std::uniform_int_distribution<std::int64_t> dist{2, p - 1};
     std::default_random_engine gen{std::random_device{}()};
 
     auto const actualWitnessCount{std::min(randomWitnessCount, p - 1)};
 
-    for (T i{0}; i < actualWitnessCount; ++i) {
+    for (std::int64_t i{0}; i < actualWitnessCount; ++i) {
       if (!testWitness(dist(gen))) {
         return false;
       }
@@ -134,7 +130,7 @@ template <std::integral T>
 }
 
 auto main() -> int {
-  for (auto i{3ull}; i <= 50; i++) {
+  for (auto i{3ll}; i <= 50; i++) {
     std::cout << i << ": " << MillerRabin(i, MillerRabinMode::TestAll) << '\n';
   }
 }
