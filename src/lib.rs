69ba93fd08420b0984a19f73c37a1b335152b17d
Levente Löffler <levente.loffler99@gmail.com>
2024-01-20 16:12:19 +0100

    Add initial miller-rabin test

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,22 @@
 use num::traits::One;
 use num::traits::Pow;
 use num::traits::Zero;
-use num::BigInt;
+use num::BigUint;
 
-fn fast_pow(base: &BigInt, exp: &BigInt) -> BigInt {
-    let zero: BigInt = BigInt::zero();
-    let one: BigInt = BigInt::one();
+fn fast_pow(base: &BigUint, exp: &BigUint) -> BigUint {
+    let zero: BigUint = BigUint::zero();
+    let one: BigUint = BigUint::one();
 
     if *exp <= zero {
-        return BigInt::zero();
+        return BigUint::zero();
     }
 
-    let mut mul = BigInt::from(1);
+    let mut mul = BigUint::from(1u8);
     let mut base = base.clone();
     let mut exp = exp.clone();
 
     while exp > one {
-        if &exp % 2 == one {
+        if &exp % 2u8 == one {
             mul *= &base;
             exp -= &one;
         }
@@ -28,9 +28,9 @@ fn fast_pow(base: &BigInt, exp: &BigInt) -> BigInt {
     base * mul
 }
 
-fn fast_mod_pow(base: &BigInt, exp: &BigInt, modulus: &BigInt) -> BigInt {
-    let zero: BigInt = BigInt::zero();
-    let one: BigInt = BigInt::one();
+fn fast_mod_pow(base: &BigUint, exp: &BigUint, modulus: &BigUint) -> BigUint {
+    let zero: BigUint = BigUint::zero();
+    let one: BigUint = BigUint::one();
 
     if *modulus == zero {
         return zero;
@@ -41,7 +41,7 @@ fn fast_mod_pow(base: &BigInt, exp: &BigInt, modulus: &BigInt) -> BigInt {
     let mut exp = exp.clone();
 
     while exp > zero {
-        if &exp % 2 == one {
+        if &exp % 2u8 == one {
             ret = ret * &base % modulus;
         }
 
@@ -52,6 +52,49 @@ fn fast_mod_pow(base: &BigInt, exp: &BigInt, modulus: &BigInt) -> BigInt {
     ret
 }
 
+fn miller_rabin(p: &BigUint) -> bool {
+    let two = BigUint::from(2u8);
+
+    if p == &two {
+        return true;
+    }
+
+    let zero = BigUint::zero();
+    let one = BigUint::one();
+
+    let p_minus_one = p - &one;
+    let mut m = p_minus_one.clone();
+    let mut r: u64 = 0;
+
+    while &m % &two == zero {
+        m /= &two;
+        r += 1;
+    }
+
+    let m = m;
+    let r = r;
+    let mut a = one.clone();
+
+    // Initializing to true because a^m mod p == 1 results in a pass
+    let mut prev_was_minus_one = true;
+
+    while &a < &p {
+        for i in 0..=r {
+            let x = fast_mod_pow(&a, &(&m * &fast_pow(&two, &i.into())), &p);
+
+            if prev_was_minus_one && x == one {
+                return true;
+            }
+
+            prev_was_minus_one = x == p_minus_one;
+        }
+
+        a += 1u8;
+    }
+
+    false
+}
+
 #[cfg(test)]
 mod tests {
     use crate::*;
@@ -59,16 +102,29 @@ mod tests {
     #[test]
     fn fast_pow_test() {
         assert_eq!(
-            fast_pow(&BigInt::from(5), &BigInt::from(2)),
-            BigInt::from(25)
+            fast_pow(&BigUint::from(5u8), &BigUint::from(2u8)),
+            BigUint::from(25u8)
         );
     }
 
     #[test]
     fn fast_mod_pow_test() {
         assert_eq!(
-            fast_mod_pow(&BigInt::from(5), &BigInt::from(2), &BigInt::from(3)),
-            BigInt::from(1)
+            fast_mod_pow(
+                &BigUint::from(5u8),
+                &BigUint::from(2u8),
+                &BigUint::from(3u8)
+            ),
+            BigUint::from(1u8)
         );
+    }
+
+    #[test]
+    fn miller_rabin_test() {
+        for p in [
+            2u16, 3, 5, 7, 23, 383, 1031, 2087, 3359, 4447, 5519, 6329, 7919,
+        ] {
+            assert!(miller_rabin(&p.into()));
+        }
     }
 }
